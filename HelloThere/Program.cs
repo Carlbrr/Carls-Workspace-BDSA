0b27908c7f5fa87284921c483894bde749e3849f
Carlbrr <90042145+Carlbrr@users.noreply.github.com>
2021-09-06 16:51:29 +0200

    Implemented error handling (#4)
    Co-authored-by: Carl <carbr@itu.dk>

--- HelloThere/Program.cs
+++ HelloThere/Program.cs
@@ -4,15 +4,25 @@ namespace HelloThere
 {
     public class Program
     {
-        public static void Main(string []args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a year and press the Enter key: ");
-            int year = Convert.ToInt32(Console.ReadLine());
-            if(isLeapYear(year)){
-                Console.WriteLine("yay");
-            }else{
-                Console.WriteLine("nay");
+            Console.WriteLine("Please type a year and press the Enter key: ");
+            Console.WriteLine("- note that you must input a year above 1582 in a four digit format: yyyy");
+            try{
+                int year = Convert.ToInt32(Console.ReadLine());
+                if(year < 1582){
+                    throw new ArgumentException("The typed year must be above 1582"); 
+                }
+                if(isLeapYear(year)){
+                    Console.WriteLine("yay");
+                }else{
+                    Console.WriteLine("nay");
+                }
+            }catch(Exception e){
+                Console.WriteLine(e.Message);
+                Console.Write(" - Try again!");
             }
+            
 
         }
         public static bool isLeapYear(int year)
