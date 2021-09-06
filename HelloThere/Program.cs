ca1047b6b03bacef8c22e3a21e767393938eeee9
Carl <carbr@itu.dk>
2021-09-06 15:58:54 +0200

    Tested and implemented isLeapYear

--- HelloThere/Program.cs
+++ HelloThere/Program.cs
@@ -2,11 +2,35 @@
 
 namespace HelloThere
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string []args)
         {
-            Console.WriteLine("Hello World!");
         }
+        public static bool isLeapYear(int year)
+        {
+            if(year % 4 == 0)
+            {
+                if(year % 100 == 0)
+                {
+                    if(year % 400 == 0)
+                    {
+                        return true;
+                    }else
+                    {
+                      return false;  
+                    }  
+                }else
+                {
+                    return true;
+                }
+            }else
+            {
+                return false;
+            }
+
+        }
+        
+   
     }
 }
